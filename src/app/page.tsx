7d68ec09b06c71781f5e624b68ba03cbd973d9a3
lukatjee <luka.stoop@outlook.com>
2025-10-26 13:55:48 +0100

    fix: Updated the link for the Discord server.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
                     <div className="flex gap-4 items-center flex-col sm:flex-row">
                         <a
                             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                            href="https://discord.gg/qZVzXqUKZR"
+                            href="https://discord.gg/zZGGU4Q6XK"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
